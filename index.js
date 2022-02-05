478c0126bcd71b9245a72bab46599a7d8284d873
jordanfox1 <jordanfox9415@gmail.com>
2022-02-06 00:29:24 +1000

    image upload working on client and server side

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express")
 const app = express();
 const cors = require("cors")
-
 const Pool = require("pg").Pool;
+const { cloudinary } = require('./utils/cloudinary')
 
-//Credentials
+
+// Postgres Credentials
 const pool = new Pool({
     user: "postgres",
     password: "postgres",
@@ -13,14 +14,27 @@ const pool = new Pool({
     database: "gyeme"
 })
 
-app.listen(5000, () => {
-    console.log('Server has started on port 5000')
-})
+
 
 app.use(cors())
-app.use(express.json())
+app.use(express.json({ limit: '50mb'}))
+app.use(express.urlencoded({extended: true}))
 
 //ROUTES:
+
+//image uploading
+app.post('/api/upload', async (req, res)=> {
+    try {
+        const fileString = req.body.data
+        const uploadedResponse = await cloudinary.uploader.upload(fileString, {
+            upload_preset: 'default_unsigned'
+        })
+        console.log(uploadedResponse)
+        res.json({msg: "congratualtions!!! you've uploaded an image"})
+    } catch (error) {
+        console.error(error)
+    }
+})
 
 //create a class
 app.post("/classes", async (req, res) =>{
@@ -132,4 +146,8 @@ app.get("/users", async (req, res) => {
     } catch (error) {
         console.error(error.message)
     }
+})
+
+app.listen(5000, () => {
+    console.log('Server has started on port 5000')
 })
